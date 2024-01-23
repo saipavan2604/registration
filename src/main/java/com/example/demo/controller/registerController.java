4185a4d93cdaf7a7ea1e1483574845698a433487
saipavan sonimi <saipavan2604@gmail.com>
2024-01-23 16:42:09 -0600

    y f1 registration files name login and resitration

---
+++ src/main/java/com/example/demo/controller/registerController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class registerController{
+
+	
+	@GetMapping("/")
+	public String homepage() {
+	    return "index";
+	  }
+
+	
+}
